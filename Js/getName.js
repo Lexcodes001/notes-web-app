43bdc4eacf49835d2da8b917108cd6d626e5d92a
adetayoalexander12@gmail.com <adetayoalexander12@gmail.com>
2022-09-05 21:30:01 +0100

    Project ongoing...

--- Js/getName.js
+++ Js/getName.js
@@ -3,15 +3,60 @@ var grts = document.getElementById('grts');
 var btnOne = document.getElementById('btn-one');
 var btnTwo = document.getElementById('btn-two');
 btnTwo.style.visibility = 'hidden';
-    
+btnOne.addEventListener("click", getName);
+btnTwo.addEventListener("click", goToPage);
+
 function getName() {
-    var name = document.getElementById('name').value;
-    if (name == '') {
+    var getName = document.getElementById('name').value.trim();
+    localStorage.setItem("name", getName);
+    var userName = localStorage.getItem("name");
+    if (userName == '') {
         grts.innerHTML = 'Welcome here Buddy!';
     } else {
-        grts.innerHTML = 'Welcome here ' + name + '!';
+        grts.innerHTML = `Welcome here ${userName}!`;
     }
     nameCnt.style.display = 'none';
     btnOne.style.display = 'none';
     btnTwo.style.visibility = 'visible';
+}
+
+function goToPage() {
+    var getName = document.getElementById('name').value.trim();
+    localStorage.setItem("name", getName);
+    var userName = localStorage.getItem("name");
+
+    let mainPage = `<div class="body">
+            
+    <div class="topnav">
+        <div class="topnav-left">
+            <div onclick="displayNotes()" class="topnav-left-item">
+                Notes
+            </div>
+            <div onclick="displayTodolist()" class="topnav-left-item">
+                To-Do List
+            </div>
+        </div>
+
+        <div class="topnav-middle">${userName}</div>
+
+        <div class="topnav-right">
+            <span class="material-icons">facebook</span>
+            <span ><img class="icon" src="/Images/whatsapp-icon.png" alt="whatsapp icon" /></span>
+            <span><input type="checkbox" id="toggle">
+            <label for="toggle" class="button"></label></span>
+        </div>
+    </div>
+
+    <div id="bodycontent">
+        <div id="notes">notes</div>
+        <div id="todolist">todolist</div>
+    </div>
+
+</div>
+
+<script src="/Js/getName.js"></script>
+<script src="/Js/toggle.js"></script>
+<script src="/Js/actions.js"></script>`;
+
+document.querySelector('body').innerHTML = mainPage;
 }
---
+++ Js/toggle.js
@@ -0,0 +1,16 @@
+var size = 8;
+
+var board = '';
+
+for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+        board += ' ';
+    } else {
+        board += '#';
+    }
+    board += '/n';
+    }
+    console.log(board);
+}
+
